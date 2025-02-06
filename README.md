1d682ccd2e8b39bdef1d73118ea4408719d5b6be
HE_SW <khaneol@deepnoid.com>
2025-02-06 10:33:37 +0900

    fix: image domain 추가

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
 모델 설치
 ```
-    wget --no-clobber https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth -O model/sam_vit_b_01ec64.pth
+    wget --no-clobber https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth -O model/sam_vit_h_4b8939.pth
 ```
--- controller/ai/controller.py
+++ controller/ai/controller.py
@@ -13,10 +13,8 @@ async def get_npy_by_img_path(
     image_url:str,
     service: AiService = Depends(Provide[AiContainer.ai_service]),
 ):  
-    print('image path', image_url)
     npy_bytes = service.get_image_npy_bytes(image_path=image_url)
-    # filename = image_url.replace('.jpg', '.npy') if image_url.endswith('.jpg') else "embedding.npy"
-    filename =  "embedding.npy"
+    filename = image_url.replace('.jpg', '.npy') if image_url.endswith('.jpg') else "embedding.npy"
 
     return Response(
         content=npy_bytes,
